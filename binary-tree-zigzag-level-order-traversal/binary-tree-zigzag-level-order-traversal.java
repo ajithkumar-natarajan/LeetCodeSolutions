1420dc9966574b3f60bd4763532499547f61a766
Ajith Kumar Natarajan <natarajan.ajithkumar@gmail.com>
2021-03-29 19:38:31 -0400

    Time: 1 ms (74.96%), Space: 38.9 MB (82.18%) - LeetHub

--- binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -15,36 +15,68 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+//         if(root == null)
+//             return new ArrayList<>();
+       
+//         List<List<Integer>> ans = new ArrayList<>();
+//         Queue<TreeNode> q = new LinkedList<>();
+//         int height = 0;
+       
+//         q.offer(root);
+       
+//         while(!q.isEmpty()){
+//             int n = q.size();
+//             List<Integer> level = new ArrayList<>();
+           
+//             for(int i=0; i<n; ++i){
+//                 TreeNode node = q.poll();
+//                 level.add(node.val);
+               
+//                 if(node.left != null)
+//                     q.offer(node.left);
+//                 if(node.right != null)
+//                     q.offer(node.right);
+               
+//             }
+//             if(height % 2 != 0)
+//                 Collections.reverse(level);                
+           
+//             ans.add(level);
+//             ++height;
+//         }
+       
+//         return ans;
+        
         if(root == null)
             return new ArrayList<>();
-       
+        
         List<List<Integer>> ans = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        int height = 0;
-       
+        Deque<TreeNode> q = new ArrayDeque<>();
         q.offer(root);
-       
+        boolean flag = true;
+        
         while(!q.isEmpty()){
-            int n = q.size();
-            List<Integer> level = new ArrayList<>();
-           
-            for(int i=0; i<n; ++i){
-                TreeNode node = q.poll();
-                level.add(node.val);
-               
+            List<Integer> inner = new ArrayList<>();
+            int len = q.size();
+            
+            for(int i=0; i<len; ++i){
+                TreeNode node = q.pop();
+                inner.add(node.val);
                 if(node.left != null)
                     q.offer(node.left);
                 if(node.right != null)
                     q.offer(node.right);
-               
             }
-            if(height % 2 != 0)
-                Collections.reverse(level);                
-           
-            ans.add(level);
-            ++height;
+            if(!flag){
+                flag = true;
+                Collections.reverse(inner);
+                ans.add(inner);
+            }
+            else{
+                flag = false;
+                ans.add(inner);
+            }
         }
-       
         return ans;
     }
 }
