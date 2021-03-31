f01c3fd9bd2464cca7c83fb73241a4dde7b5b99a
Ajith Kumar Natarajan <natarajan.ajithkumar@gmail.com>
2021-03-31 00:22:22 -0400

    Time: 4 ms (80.48%), Space: 40.4 MB (81.65%) - LeetHub

--- merge-k-sorted-lists/merge-k-sorted-lists.java
+++ merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,26 +10,66 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        List<Integer> list = new ArrayList<>();
-        
-        for(ListNode node: lists){
-            // System.out.println(node.val);
-            while(node != null){
-                list.add(node.val);
-                node = node.next;
-            }
-        }
-        Collections.sort(list);
-        // System.out.println(list);
-        
-        ListNode ans = new ListNode();
+        ListNode ans = new ListNode(0);
         ListNode ret = ans;
-        for(int i: list){
-            ans.next = new ListNode(i);
-            ans = ans.next;
-            // ans.val = i;
+        
+        if(lists.length == 0)
+            return ans.next;
+        
+        PriorityQueue<ListNode> q = new PriorityQueue<>(lists.length, new Comparator<ListNode>(){
+            public int compare(ListNode o1, ListNode o2){
+                if(o1.val < o2.val)
+                    return -1;
+                else if(o1.val > o2.val)
+                    return 1;
+                return 0;
+            }
+        });
+        
+        for(ListNode l: lists){
+            if(l != null)
+                q.offer(l);
         }
+        
+        // for(ListNode l: q)
+        //     System.out.println(l.val);
+        
+        while(!q.isEmpty()){
+            ListNode node = q.poll();
+            ans.next = new ListNode(node.val);
+            ans = ans.next;
+            
+            if(node.next != null)
+                q.offer(node.next);
+        }
+        
         
         return ret.next;
+        
+        
+        
+        
+        // List<Integer> list = new ArrayList<>();
+        
+        // for(ListNode node: lists){
+        //     System.out.println(node.val);
+        //     while(node != null){
+        //         list.add(node.val);
+        //         node = node.next;
+        //     }
+        // }
+//         Collections.sort(list);
+        // System.out.println(list);
+        
+//         ListNode ans = new ListNode();
+//         ListNode ret = ans;
+//         for(int i: list){
+//             ans.next = new ListNode(i);
+//             ans = ans.next;
+//             // ans.val = i;
+//         }
+        
+        // return lists[0];
+        // return ret.next;
     }
 }
