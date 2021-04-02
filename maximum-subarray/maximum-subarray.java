55df416e665990bf0aee442de97daa31cd627b7c
Ajith Kumar Natarajan <natarajan.ajithkumar@gmail.com>
2021-04-01 23:29:58 -0400

    Time: 0 ms (100.00%), Space: 38.9 MB (59.74%) - LeetHub

---
+++ maximum-subarray/maximum-subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int sum = nums[0];
+        for(int i=1; i<nums.length; ++i){
+            if(sum < 0)
+                sum = 0;
+            sum += nums[i];
+            max = Math.max(max, sum);
+        }
+        
+        return max;
+    }
+}
