ad5f30982f274a0e79000f2449e1374c21aa666b
Ajith Kumar Natarajan <natarajan.ajithkumar@gmail.com>
2021-04-05 08:25:18 -0400

    Time: 0 ms (100.00%), Space: 40.2 MB (23.22%) - LeetHub

---
+++ binary-search/binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l=0, r=nums.length;
+        
+        while(l<r){
+            int mid = l+(r-l)/2;
+            
+            if(nums[mid]==target)
+                return mid;
+            
+            if(nums[mid]>target)
+                r=mid;
+            else
+                l=mid+1;
+        }
+        
+        return -1;
+    }
+}
