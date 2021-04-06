a027756aa35506c0356ee503fa56ef5b4438afd3
Ajith Kumar Natarajan <natarajan.ajithkumar@gmail.com>
2021-04-06 13:48:02 -0400

    Time: 5 ms (94.40%), Space: 41.6 MB (68.09%) - LeetHub

--- merge-intervals/merge-intervals.java
+++ merge-intervals/merge-intervals.java
@@ -2,56 +2,35 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         if(intervals.length == 1)
             return intervals;
-        PriorityQueue<int[]> q = new PriorityQueue<>(new sort());
-        // PriorityQueue<Integer> q = new PriorityQueue<>();
+        
+        // PriorityQueue<int[]> q = new PriorityQueue<>(new sort());
         List<int[]> sorted = new ArrayList<>();
         List<int[]> ans = new ArrayList<>();
         
         
         for(int[] i: intervals)
-            // q.offer(i[0]);
-            q.offer(i);
+            // q.offer(i);
+            sorted.add(i);
         
-        // Iterator<int[]> iter = q.iterator();
-        while(!q.isEmpty()){
-            // int[] i = q.poll();
-            // System.out.println(Arrays.toString(i));
-            sorted.add(q.poll());
-        }
-        // for(int[] i: sorted)
-        //     System.out.println(Arrays.toString(i));
+        Collections.sort(sorted, new sort());
+        
+        // while(!q.isEmpty()){
+        //     sorted.add(q.poll());
+        // }
         
         for(int i=0; i<sorted.size(); ++i){
             int[] a = sorted.get(i);
-            // boolean flag = false;
             int max = a[1];
-            // int[] b = sorted.get(i+1);
             
-//             if(b[0] <= a[1]){
-                
-//             }
             while(i<sorted.size()-1 && max >= sorted.get(i+1)[0]){
                 max = Math.max(max, sorted.get(i+1)[1]);
                 ++i;
-                // System.out.println(max);
-                // flag = true;
             }
             
-            // if(i<sorted.size() && flag){
-                a[1] = max;
-                // --i;
-            // }
+            a[1] = max;
+            
             ans.add(a);
-            // for(int[] arr: ans)
-            //     System.out.print(Arrays.toString(arr));
-            // System.out.println();
         }
-        
-        // Iterator<Integer> iter = q.iterator();
-        // while(!q.isEmpty()){
-            // int i = iter.next();
-            // System.out.println(q.poll());
-        // }
         
         return ans.toArray(new int[0][0]);
     }
