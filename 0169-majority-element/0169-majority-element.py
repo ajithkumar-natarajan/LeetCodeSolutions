b2a088faa1ab84d1e12efa08deb1d38a59d98209
Ajith Kumar Natarajan <natarajan.ajithkumar@gmail.com>
2026-02-16 00:44:39 -0800

    Time: 3 ms (85.58%), Space: 21.4 MB (15.71%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,32 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count_map = dict()
-        length = len(nums)
+        # count_map = dict()
+        # length = len(nums)
 
-        for n in nums:
-            count = count_map.get(n, 0)
-            count += 1
-            count_map[n] = count
+        # for n in nums:
+        #     count = count_map.get(n, 0)
+        #     count += 1
+        #     count_map[n] = count
 
-            if count > length//2:
-                return n
+        #     if count > length//2:
+        #         return n
 
-        return 0
+        # return 0
+
+        count = 1
+        res = nums[0]
+
+        for n in nums[1:]:
+            if n == res:
+                count += 1
+            else:
+                count -= 1
+            
+            if count < 0:
+                count = 0
+                
+            if count == 0:
+                res = n
+                count += 1
+
+        return res
