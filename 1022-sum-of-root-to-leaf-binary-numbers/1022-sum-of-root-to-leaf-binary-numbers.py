1ed5ce82a8c29b83ac6ce8dc92045819aaebe1ca
Ajith Kumar Natarajan <natarajan.ajithkumar@gmail.com>
2026-03-31 15:59:12 -0700

    Time: 3 ms (23.24%), Space: 19.5 MB (92.26%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -7,18 +7,52 @@
 class Solution:
     def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
 
-        def dfs(node, n):
-            if not node:
-                return
+        # def dfs(node, n):
+        #     if not node:
+        #         return
 
-            n = 2*n + node.val
+        #     n = 2*n + node.val
+        #     if not node.left and not node.right:
+        #         self.ans += n
+
+        #     dfs(node.left, n)
+        #     dfs(node.right, n)
+
+        # self.ans = 0
+        # dfs(root, 0)
+
+        # return self.ans
+
+        # Enter your code here. Read input from STDIN. Print output to STDOUT
+        """
+        # ORACLE INTERVIEW:
+                0
+            1   1
+            0 1
+        010 + 011 + 01
+
+        [[0]] -> [[010, 010]]
+
+        [0, 1, 0]
+
+        """
+
+        total = 0
+        def dfs_sum(node, binary):
+            nonlocal total
+            binary.append(str(node.val))
             if not node.left and not node.right:
-                self.ans += n
+                binary_string = ''.join(binary)
+                total += int(binary_string, 2)
+            if node.left:
+                dfs_sum(node.left, binary) #-> binary=[0, 1, 0], total=2
+            if node.right:
+                dfs_sum(node.right, binary)
+            
+            binary.pop()
 
-            dfs(node.left, n)
-            dfs(node.right, n)
+        if not root:
+            return 0
+        dfs_sum(root, [])
 
-        self.ans = 0
-        dfs(root, 0)
-
-        return self.ans
+        return total
