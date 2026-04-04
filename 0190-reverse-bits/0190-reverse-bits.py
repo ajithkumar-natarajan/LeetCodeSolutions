5ce85c5883a38bdf8644866c93ae3dac4918cc9c
Ajith Kumar Natarajan <natarajan.ajithkumar@gmail.com>
2026-04-04 13:12:50 -0700

    Time: 30 ms (99.64%), Space: 19.5 MB (10.48%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,4 +1,12 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        given = ''.join(['0']*(32-len(str(bin(n))[2:])))+str(bin(n)[2:])
-        return int(given[::-1], base=2)
+        # given = ''.join(['0']*(32-len(str(bin(n))[2:])))+str(bin(n)[2:])
+        # return int(given[::-1], base=2)
+
+        rev = ['0']*32
+        b = str(bin(n))[2::][::-1]
+        
+        for idx, c in enumerate(b):
+            rev[idx] = c
+
+        return int(''.join(rev), 2)
