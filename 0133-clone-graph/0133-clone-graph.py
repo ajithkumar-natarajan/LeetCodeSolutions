265428b5e26efc8be1bb74da8c9c04aed4cafb45
Ajith Kumar Natarajan <natarajan.ajithkumar@gmail.com>
2026-04-05 14:05:26 -0700

    Time: 52 ms (43.97%), Space: 19.8 MB (35.08%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -14,24 +14,21 @@ class Solution:
         
         node_dict = {}
         visited = set()
-        val = 1
-        max_val = 1
         q = deque([node])
 
         while q:
             n = q.popleft()
             if n.val not in node_dict:
                 node_dict[n.val] = Node(n.val)
+            neigh = []
             for neighbor in n.neighbors:
+                if neighbor.val not in node_dict:
+                    node_dict[neighbor.val] = Node(neighbor.val)
+                neigh.append(node_dict[neighbor.val])
                 if neighbor not in visited:
                     q.append(neighbor)
                     visited.add(neighbor)
-
-        for n in visited:
-            neighbor = []
-            for no in n.neighbors:
-                neighbor.append(node_dict[no.val])
-            node_dict[n.val].neighbors = neighbor
+            node_dict[n.val].neighbors = neigh
 
 
         return node_dict[1]
